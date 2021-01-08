827a3b6e216c57792929bebb89f992c12693405c
ananth <ananthkvmohanan@gmail.com>
2021-01-08 12:46:08 +0530

    fixed some issues

--- main.go
+++ main.go
@@ -3,31 +3,53 @@ package main
 import (
 	"crypto/tls"
 	"fmt"
-	"io/ioutil"
 	"net/http"
 	"os"
+
+	"github.com/steelx/extractlinks"
+)
+
+var (
+	config = &tls.Config{
+		InsecureSkipVerify: true,
+	}
+	transport = &http.Transport{
+		TLSClientConfig: config,
+	}
+	netClient = &http.Client{
+		Transport: transport,
+	}
 )
 
 func main() {
-	fmt.Printf("hii")
-	config := &tls.Config{
-		InsecureSkipVerify: true,
-	}
-	transport := &http.Transport{
-		TLSClientConfig: config,
-	}
-	netClient := &http.Client{
-		Transport: transport,
-	}
-	baseURL := "https://youtube.com/pewdiepie"
 
-	response, err := netClient.Get(baseURL)
+	arguments := os.Args[1:]
+	if len(arguments) == 0 {
+		fmt.Println("Provide a url")
+		os.Exit(1)
+	}
+
+	baseURL := arguments[0]
+	fmt.Println(baseURL)
+
+	getURL(baseURL)
+
+}
+
+func getURL(href string) {
+	fmt.Printf(href)
+	response, err := netClient.Get(href)
+	checkError(err)
+	defer response.Body.Close()
+
+	links, err := extractlinks.All(response.Body)
+
 	checkError(err)
 
-	body, _ := ioutil.ReadAll(response.Body)
-	fmt.Println(string(body))
-
-	response.Body.Close()
+	for _, link := range links {
+		// fmt.Printf("index %v -- link %+v \n", i, link)
+		getURL(link.Href)
+	}
 }
 
 func checkError(err error) {
